568531e47de3715875e47fabaee5b6f5181920d2
Jose Rojas Paredes <jrojasp1992@gmail.com>
2020-04-13 12:34:38 -0500

    asd

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+/* function Helloworld(props) { 
+return(
+<div id="hola">
+<h3>{props.subtitle}</h3>
+{props.mytext}
+</div>
+) 
+ } */
+class Helloworld extends React.Component{
+
+state = {
+  show: false
+}
+
+  render(){
+    if (this.state.show){
+      return(
+        <div id="hola">
+        <h3>{this.props.subtitle}</h3>
+        {this.props.mytext}
+        </div>
+      )
+    }else{
+      return <h1>There are not elements</h1>
+    }
+    
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      This is may component:
+      <Helloworld mytext="Hello Fazt" subtitle="loremp ispum"/>
+       <Helloworld mytext="Hola mundo" subtitle="asadsad"/>
+        <Helloworld mytext="Hello!" subtitle="component three"/>
+        </div>
   );
 }
+
 
 export default App;
